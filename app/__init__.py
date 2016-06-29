2e17d42932aad23e8abad6e8e39b9b08ffa5ddfe
Patrick Lee <me@patricklee.nyc>
2016-06-29 08:04:49 -0400

    Update __init__.py
    Updated flask delimiters so Vue.js works as intended.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,19 @@
 from flask import Flask
 
-app = Flask(__name__)
+
+# Need to change Flask Jinja 2 delimiters so vue.js works without modification.
+# https://gist.github.com/lost-theory/3925738
+class CustomFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='<%',
+        block_end_string='%>',
+        variable_start_string='%%',
+        variable_end_string='%%',
+        comment_start_string='<#',
+        comment_end_string='#>',
+    ))
+
+app = CustomFlask(__name__)
 from app import views
 
