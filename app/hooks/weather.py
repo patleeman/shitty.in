de9c60242d4c7371e1d48b1077aa773b0cc34f11
Patrick Lee <me@patricklee.nyc>
2016-07-04 13:26:49 -0400

    updated weather with additional info, tweaked weather score, tweaked styles

--- app/hooks/weather.py
+++ app/hooks/weather.py
@@ -15,8 +15,10 @@ class Weather(object):
 
 
     def get_data(self):
-        currently = self._get_data()['currently']
-        self.payload = self._process_currently(currently)
+        data = self._get_data()
+        currently = data['currently']
+        today = data['daily']['data'][0]
+        self.payload = self._process_weather(currently, today)
         return self.payload
 
     def _get_data(self):
@@ -25,47 +27,66 @@ class Weather(object):
         return data
 
 
-    def _process_currently(self, currently):
+    def _process_weather(self, currently, today):
         optimal_humidity = [.4, .6]
         optimal_temp = [60, 80]
 
         current_temp = currently['apparentTemperature']
+        high_temp = today['temperatureMax']
+        low_temp = today['temperatureMin']
         current_humidity = currently['humidity']
-        current_precip = currently['precipProbability']
+        precip = today['precipProbability']
 
         payload = {}
         payload['temp'] = {'value': int(current_temp)}
+        payload['high_temp'] = {'value': int(high_temp)}
+        payload['low_temp'] = {'value': int(low_temp)}
         payload['humidity'] = {'value': int(100 * current_humidity)}
-        payload['precip'] = {'value': current_precip}
+        payload['precip'] = {'value': int(100* precip)}
 
-        # Calculate temp score
-        if current_temp >= optimal_temp[0] and current_temp <= optimal_temp[1]:
-            temp_score = 0
-        elif current_temp < optimal_temp[0]:
-            temp_score = (optimal_temp[0] - current_temp) / optimal_temp[0]
-        elif current_temp > optimal_temp[1]:
-            temp_score = (current_temp - optimal_temp[1]) / optimal_temp[1]
-        else:
-            raise ValueError("Temp score calculation outside calculable ranges.")
+        # Calculate current temp score
+        temp_score = self._calculate_weather_score(current_temp, optimal_temp[0], optimal_temp[1])
         payload["temp"]['score'] = int(temp_score * 100)
 
+        # Calculate high temp score
+        high_temp_score = self._calculate_weather_score(high_temp, optimal_temp[0], optimal_temp[1])
+        payload["high_temp"]['score'] = int(high_temp_score * 100)
+
+        # Calculate low temp score
+        low_temp_score = self._calculate_weather_score(low_temp, optimal_temp[0], optimal_temp[1])
+        payload["low_temp"]['score'] = int(low_temp_score * 100)
+
         # Calculate humidity score
-        if current_humidity >= optimal_humidity[0] and current_humidity <= optimal_humidity[1]:
-            humidity_score = 0
-        elif current_humidity > optimal_humidity[1]:
-            humidity_score = (current_humidity - optimal_humidity[1]) / optimal_humidity[1]
-        elif current_humidity < optimal_humidity[0]:
-            humidity_score = (optimal_humidity[0] - current_humidity) / optimal_humidity[0]
-        else:
-            raise ValueError("Temp score calculation outside calculable ranges.")
+        humidity_score = self._calculate_weather_score(current_humidity, optimal_humidity[0], optimal_humidity[1])
         payload['humidity']['score'] = int(humidity_score * 100)
 
         # Calculate precip score
-        precip_score = payload['precip']['value']
-        payload["precip"]['score'] = int(precip_score * 100)
+        payload["precip"]['score'] = int(precip * 100)
+        payload['precip']['type'] = today.get('precipType', '')
 
-        total_score = (temp_score * .40) + (humidity_score * .30) + (precip_score * .30)
+        total_score = (temp_score * .15) \
+                      + (high_temp_score * .20) \
+                      + (low_temp_score * .05) \
+                      + (humidity_score * .20) \
+                      + (precip * .40)
 
         payload['score'] = int(total_score * 100)
 
+        # Additional items
+        payload['current_summary'] = currently['summary']
+        payload['today_summary'] = today['summary']
+
         return payload
+    
+    @staticmethod
+    def _calculate_weather_score(metric, min, max):
+        if metric >= min and metric <= max:
+            score = 0
+        elif metric < min:
+            score = (min - metric) / min
+        elif metric > max:
+            score = (metric - max) / max
+        else:
+            raise ValueError("Temp score calculation outside calculable ranges.")
+
+        return score
